3a7b53d573c42276961304226fe071b8ae6e7a42
cuiyalei <cuiyalei163@163.com>
2022-07-05 19:38:54 +0800

    修复菜单偶发性失焦无法消失问题

--- src/view/menu.vue
+++ src/view/menu.vue
@@ -31,44 +31,55 @@ export default {
     },
   },
   async mounted() {
-    width = 180;
-    height = 150;
-    window.native.window.width = width;
-    window.native.window.height = height;
-    window.native.window.hideInTaskView();
-    window.native.window.onMessage = (type, value) => {
-      console.log(value);
-      if (type == 0) {
-        left = value.x - width / 2;
-        top = value.y - height;
-        window.native.window.left = left;
-        window.native.window.top = top;
-        this.show = true;
-      } else if (type == 10) {
-        window.dispatchEvent(new CustomEvent("config"));
-      }
-    };
-    window.native.window.onDeactivated(() => {
-      this.show = false;
-    });
-    window.native.device.mouse.addGlobalEvent((type, button, x, y) => {
-      if (
-        type > -1 &&
-        type != 5 &&
-        (left > x || left + width < x || top > y || top + height < y)
-      ) {
-        this.show = false;
-      } else if (type == 0) {
-        console.log(x, x);
-      }
-    });
-    setTimeout(() => {
-      window.native.device.mouse.initGlobalEvent();
-    }, 2000);
+    if (window.native && window.native.isInit) {
+      this.init();
+    } else {
+      window.addEventListener("native", this.init);
+    }
   },
   methods: {
+    init() {
+      width = 180;
+      height = 150;
+      window.native.window.width = width;
+      window.native.window.height = height;
+      window.native.window.hideInTaskView();
+      window.native.window.onMessage = (type, value) => {
+        console.log(value);
+        if (type == 0) {
+          left = value.x - width / 2;
+          top = value.y - height;
+          window.native.window.left = left;
+          window.native.window.top = top;
+          this.show = true;
+        } else if (type == 10) {
+          window.dispatchEvent(new CustomEvent("config"));
+        }
+      };
+      window.native.window.onDeactivated(() => {
+        this.show = false;
+      });
+      window.native.device.mouse.addGlobalEvent((type, button, x, y) => {
+        if (
+          type > -1 &&
+          type != 5 &&
+          (left > x || left + width < x || top > y || top + height < y)
+        ) {
+          this.show = false;
+        } else if (type == 0) {
+          console.log(x, x);
+        }
+      });
+      setTimeout(() => {
+        window.native.device.mouse.initGlobalEvent();
+      }, 2000);
+    },
     selectItem(i) {
-      window.native.window.parent.onMessage({ source: "menu", type: 0, value: i });
+      window.native.window.parent.onMessage({
+        source: "menu",
+        type: 0,
+        value: i,
+      });
       this.show = false;
     },
   },

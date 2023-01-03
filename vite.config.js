9ac44add246ed1047b8539039c435793f7e97d2c
cuiyalei <cuiyalei163@163.com>
2023-01-03 20:12:42 +0800

    修复部分按钮失效

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-const { execFile,exec  } = require('node:child_process');
+const { execFile, exec } = require('node:child_process');
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-let execute=true
+let execute = true
 export default defineConfig({
   server: {
     port: 8080
   },
-  build:{
-    sourcemap:true
+  build: {
+    sourcemap: true
   },
   plugins: [vue(), AutoImport({
     resolvers: [ElementPlusResolver()],
   }),
   Components({
     resolvers: [ElementPlusResolver()],
-  }),{
-    closeBundle:(e)=>{
-        let dist=import.meta.url.substring(8,import.meta.url.lastIndexOf("/")).replaceAll('/','\\')+'\\dist';
-       execFile('bin\\SepoBuild.exe', ['res='+dist,'icon={index}','name=Som 音乐','embedded','single','output='+dist+'\\build','app.company=Som',
-      'app.product=Music','app.copyright=Som','app.trademark=Som','app.version=1.0.1.1'], (error, stdout, stderr) => {
-          console.log(error,stdout,stderr)
+  }), {
+
+    closeBundle: (e) => {
+      let dist = import.meta.url.substring(8, import.meta.url.lastIndexOf("/")).replaceAll('/', '\\') + '\\dist';
+      execFile('bin\\SepoBuild.exe', ['res=' + dist, 'icon={index}', 'name=Som 音乐', 'embedded', 'single', 'output=' + dist + '\\build', 'app.company=Som',
+        'app.product=Music', 'app.copyright=Som', 'app.trademark=Som', 'app.version=1.0.2.1'], (error, stdout, stderr) => {
+          console.log(error, stdout, stderr)
         });
-    
+
     },
-    configureServer:(e)=>{
-      if(execute)execute=false;
+    configureServer: (e) => {
+      if (execute) execute = false;
       else return;
-      let doneFn=()=>{
+      let doneFn = () => {
         execFile('bin\\SepoProgram.exe', [`http://localhost:${e.config.server.port || 3000}`], (error, stdout, stderr) => {
-          console.log(error,stdout,stderr)
+          console.log(error, stdout, stderr)
         });
       }
       doneFn();
